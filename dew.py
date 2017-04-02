82771d419d68659b4a86dded021e6a9aa5f481c5
David Buckley <dbuckl02@mail.uoguelph.ca>
2017-04-02 03:45:34 -0400

    cleaned up source a little

--- dew.py
+++ dew.py
@@ -7,9 +7,10 @@ def combine(seed, poly):
     return functools.reduce(operator.xor, bits)
 
 def expand(key, size):
+    mask = 115792089237316195423570985008687907853269984665640564039457584007913129639935 #(2^256)-1
     control = key
-    LFSR0 = key-5 & 115792089237316195423570985008687907853269984665640564039457584007913129639935 #Temporary
-    LFSR1 = key+5 & 115792089237316195423570985008687907853269984665640564039457584007913129639935 #Temporary
+    LFSR0 = key-5 & mask #Temporary
+    LFSR1 = key+5 & mask #Temporary
     expanded_key = 0
 
     LFSR0_out = 0
@@ -17,16 +18,16 @@ def expand(key, size):
     for _ in range(size):
         control_out = control >> 255 #extract MSB
         next_in = combine(control, (12, 48, 115, 133, 213, 256)) #1 + x^12 + x^48 + x^115 + x^133 + x^213 + x^256
-        control = ((control << 1) | next_in) & 115792089237316195423570985008687907853269984665640564039457584007913129639935 #Shift control 1 to the left, and insert the next_in to the LSB, then & with (2^256)-1 to cut off MSB (output)
+        control = ((control << 1) | next_in) & mask #Shift control 1 to the left, and insert the next_in to the LSB, then & with (2^256)-1 to cut off MSB (output)
 
         if control_out:
             LFSR0_out = LFSR0 >> 255
             next_in = combine(LFSR0, (12, 48, 115, 133, 213, 256)) #temporary
-            LFSR0 = ((LFSR0 << 1) | next_in) & 115792089237316195423570985008687907853269984665640564039457584007913129639935
+            LFSR0 = ((LFSR0 << 1) | next_in) & mask
         else:
             LFSR1_out = LFSR1 >> 255
             next_in = combine(LFSR1, (12, 48, 115, 133, 213, 256)) #temporary
-            LFSR1 = ((LFSR1 << 1) | next_in) & 115792089237316195423570985008687907853269984665640564039457584007913129639935
+            LFSR1 = ((LFSR1 << 1) | next_in) & mask
             
         expanded_key = (expanded_key << 1) | (LFSR0_out ^ LFSR1_out) #shift output stream one to the left, and insert new output into LSB
         
